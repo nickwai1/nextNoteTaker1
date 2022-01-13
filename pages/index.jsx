6612c99b233d113cd7bfb0120bc480f790957853
nickwai1 <53497786+nickwai1@users.noreply.github.com>
2022-01-12 22:40:01 -0800

    addpage navigatioon with Link Component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
-export default () => <h1>Index Page</h1>
+const Page = () => (
+    <div>
+        <h1>Index Page</h1>
+        <Link href="/notes">
+            <a>
+                Link
+            </a>
+        </Link>
+    </div>
+)
+
+export default Page;
--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,3 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
-export default () => <h1>Notes</h1>
+const Page = () => (
+    <div>
+        <h1>Note Index Path</h1>
+        <Link href="/notes/[id]" as={`/notes/1`}>
+            <a>
+                Note 1
+            </a>
+        </Link>
+    </div>
+)
+
+export default Page;
