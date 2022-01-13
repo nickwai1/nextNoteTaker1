3e6c537eba57faa8c6d0a532a3d895cfb4db8c20
nickwai1 <53497786+nickwai1@users.noreply.github.com>
2022-01-12 23:43:31 -0800

    theme provider

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,6 +1,6 @@
 // pages/notes/index.jsx
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 
 export default () => {
   const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `Note: ${i}`}))
